c22af6feffccdafd37c2ff56746b8a650dfa662c
Ka Hea Lam <xhianto@gmail.com>
2021-06-12 12:10:04 -0700

    Json omzetten in object

---
+++ Werkstuk2/Groep.swift
@@ -0,0 +1,18 @@
+//
+//  Groep.swift
+//  Werkstuk2
+//
+//  Created by student on 12/06/2021.
+//
+
+//import Foundation
+
+struct Groep: Codable {
+    var DATE: String
+    var REGION: String?
+    var AGEGROUP: String
+    var SEX: String?
+    var BRAND: String
+    var DOSE: String
+    var COUNT: Int
+}
--- Werkstuk2/ViewController.swift
+++ Werkstuk2/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var Groepen: [Groep]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getDataFromWebservice()
     }
-
+    
+    func getDataFromWebservice() -> Void {
+        let url = URL(string: "https://epistat.sciensano.be/Data/COVID19BE_VACC.json")!
+        print("na url maken")
+        let task = URLSession.shared.dataTask(with: url) {
+            (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            print("na sessie")
+            if let jsonData = data
+            {
+                print("na data")
+                let decoder = JSONDecoder()
+                do {
+                    self.Groepen = try decoder.decode([Groep].self, from: jsonData)
+                    //let formatter = DateFormatter()
+                    for groep in self.Groepen! {
+                        if groep.REGION != nil {
+                            print(groep.AGEGROUP)
+                            print(groep.REGION!)
+                        }
+                    }
+                }
+                catch {
+                    print(error.localizedDescription)
+                }
+            }
+            print("skipping data")
+        }
+        task.resume()
+    }
 
 }
 
